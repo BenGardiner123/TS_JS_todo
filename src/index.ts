1c0feaa93e33090cc50a5665c14af4c39e1821aa
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-05-13 21:24:22 +1000

    just needed to fix my declarations at the top

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,53 @@
 //id the divs and make them variable to use in my function
 var MakelistItem = document.getElementById("list");
-var textInput = document.getElementById("textInput");
-var makeCompleteButton = document
+var textInput = document.getElementById("textInput") as HTMLInputElement | null;
+var inputbutton = document.getElementById("inputSomethingButton") as HTMLButtonElement | null;
 
 // this function creates the list items an tells the input box to store the users input as the list string
+////need to finsih of the statment 
+if (inputbutton == null) {
+    alert("There is no button!")
+} else if (//test goes here//) {
 
-inputSomethingButton.onclick = function() {
+
+} else {
+    inputbutton.onclick = function() {
     
-    let MakelistItem = document.createElement("li");
-    let MakelistButton = document.createElement("button");
-
-    MakelistButton.innerHTML = "complete";
-
-    MakelistItem.appendChild(document.createTextNode(textInput.value));
+        let MakelistItem = document.createElement("li");
+        let MakelistButton = document.createElement("button");
     
-    list.appendChild(MakelistButton);
-    list.appendChild(MakelistItem);
-
-
-
-    //this will remove the list item and the complete button
-     
-    MakelistItem.onclick = function () {
-        list.removeChild(MakelistItem);
-        list.removeChild(MakelistButton);
-     }
-
-    //this will mark the item red for completed
-    MakelistButton.onclick = function(){
-        if (MakelistButton.style.backgroundColor == "grey")
-            {
+        MakelistButton.innerHTML = "complete";
+    
+        MakelistItem.appendChild(document.createTextNode(textInput.value));
+        
+        list.appendChild(MakelistButton);
+        list.appendChild(MakelistItem);
+    
+    
+    
+        //this will remove the list item and the complete button
+         
+        MakelistItem.onclick = function () {
+            list.removeChild(MakelistItem);
             list.removeChild(MakelistButton);
-            }
-        MakelistButton.style.backgroundColor = "red";
-
-    }
-
+         }
     
-
+        //this will mark the item red for completed
+        MakelistButton.onclick = function(){
+            if (MakelistButton.style.backgroundColor == "grey")
+                {
+                list.removeChild(MakelistButton);
+                }
+            MakelistButton.style.backgroundColor = "red";
+    
+        }
+    
+        
+    
+    }
 }
+
+
 //this button is going to remove everything from the list
 removeAllButton.onclick =  function(){
         while (list.hasChildNodes()) {  
